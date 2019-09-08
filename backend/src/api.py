fa09e1294cbf40d0296a5f00f5ca79d98aca5b2a
Thomas BAGREL <tomsb07@gmail.com>
2019-09-08 14:54:07 +0200

    Bases pour la structure
    Mise en place de la structure du projet, et des conteneurs Docker.

---
+++ backend/src/api.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, jsonify
+from src.database import db
+from src.models.user import User
+from src.models.post import Post
+
+api = Blueprint("api", __name__, url_prefix="/api/")
+
+
+@api.route("/user", methods=("GET",))
+def get_users():
+    users = User.query.all()
+    result = []
+    for user in users:
+        result.append({
+            "id": user.id,
+            "username": user.username
+        })
+    return jsonify(result)
+
+
+@api.route("/post", methods=("GET",))
+def get_posts():
+    posts = Post.query.all()
+    result = []
+    for post in posts:
+        result.append({
+            "id": post.id,
+            "title": post.title,
+            "author": post.author.username
+        })
+    return jsonify(result)
+
+
+# Testing only
+@api.route("/insert-test-data", methods=("GET",))
+def insert_test_data():
+    User.query.delete()
+    Post.query.delete()
+
+    tbagrel1 = User(id=12, username="tbagrel1", email="tbagrel1@me.org")
+    HikArrow = User(id=14, username="HikArrow", email="hik.w@joke.eu")
+    SCRUM = Post(id=2, title="SCRUM", content="Nothing there yet.", author=tbagrel1)
+
+    db.session.save(tbagrel1)
+    db.session.save(HikArrow)
+    db.session.save(SCRUM)
+    db.session.commit()
+    return "", 204
+
+
+# Testing only
+@api.route("/delete-test-data", methods=("GET",))
+def delete_test_data():
+    User.query.delete()
+    Post.query.delete()
+    db.session.commit()
+    return "", 204
+
---
+++ frontend/src/components/App.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  div#app
+    nav.navbar.navbar-dark.bg-dark
+      span.navbar-brand TAO Made Simple
+      span.navbar-text Additional interfaces for the TAO system
+    b-container#app-actions(fluid align-v="center")
+      b-row#app-row(align-h="center")
+        b-col#app-col(xs="12" sm="11" md="10" lg="9" xl="8")
+          h1 Welcome on TAO Made Simple !
+          h2 Users
+            ul
+              li(v-for="user in users" :key="user.id") {{ user.username }}
+          h2 Posts
+            ul
+              li(v-for="post in posts" :key="post.id") {{ post.title }} ({{ post.author }})
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data: () => ({
+    users: [{
+      id: -1,
+      username: '... loading ...',
+      email: ''
+    }],
+    posts: [{
+      id: -1,
+      title: '... loading ...',
+      content: '',
+      author_id: -1
+    }]
+  }),
+  mounted () {
+    this.$axios.get('http://localhost:10080/api/user')
+      .then(response => {
+        this.$jsonLog(response)
+        this.users = response.data
+      })
+    this.$axios.get('http://localhost:10080/api/post')
+      .then(response => {
+        this.$jsonLog(response)
+        this.posts = response.data
+      })
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="stylus">
+  #app
+    height 100%
+    display flex
+    flex-direction column
+    align-content center
+    justify-content start
+  #app-actions
+    flex-grow 1
+    display flex
+    flex-direction column
+    align-content center
+    justify-content center
+</style>
