5b6749a034b0f4877f33db21fd2af33987a334a9
Thomas BAGREL <tomsb07@gmail.com>
2019-11-17 15:41:49 +0100

    back fonctionnel (mocking)

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,13 +2,11 @@
 
 import os
 
-from dotenv import dotenv_values
 from flask import Flask
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_path="/var/lib/tao_made_simple")
-    app.config.from_mapping(**dotenv_values(dotenv_path="./env.env"))
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -17,12 +15,6 @@ def create_app(test_config=None):
     with app.app_context():
         from flask_cors import CORS
         cors = CORS(app)
-
-        from src.database import db
-        from src.models.user import User
-        from src.models.post import Post
-        db.init_app(app)
-        db.create_all()
 
         from src.api import api
         app.register_blueprint(api)
