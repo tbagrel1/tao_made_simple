eaf6f2387e7c3aa9b407397226e7a6780ebd5b29
Thomas BAGREL <tomsb07@gmail.com>
2019-12-03 12:05:40 +0100

    [WIP] vuex

---
+++ frontend/src/components/TestTaker.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  b-col#candidate.container.card(cols="4")
+    div#candidate-infos(@click="toggleDetailsActive()").card-body
+      div#firstname.h4.card-title {{ firstname }} ({{ id }})
+      div#status-connected(v-if="status === 1").alert.alert-success Statut : {{ statusMessage }}
+      div#status-disconnected(v-if="status === 0").alert.alert-danger Statut : {{ statusMessage }}
+      div#status-unknown(v-if="status >= 2").alert.alert-warning Statut : {{ statusMessage }}
+      b-progress(:value="questionNo" :max="nbQuestions")
+      Details(v-if="detailsActive" :type="type" :questionNo="questionNo" :startTime="startTime" :nbQuestions="nbQuestions")
+</template>
+
+<script>
+import CandidateModal from './CandidateModal'
+export default {
+  name: 'Candidates',
+  components: {
+    CandidateModal
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  }
+  // TODO: update time
+}
+</script>
+
+<style scoped lang="stylus">
+  progress-bar
+    role 'progressbar'
+    aria-valuenow 20
+    aria-valuemin 0
+    aria-valuemax 100
+</style>
---
+++ frontend/src/components/TestTakerModal.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div#details
+    div#question-no Question : {{ questionNo + 1 }} / {{ nbQuestions }}
+    div#start-time Heure de démarrage : {{ fancyStartTime }}
+</template>
+
+<script>
+export default {
+  name: 'Details',
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
